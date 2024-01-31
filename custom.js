c1149f062228ebb20589aa3cfcc8e4d53e6bb26f
Samira-K3 <you@example.comsamira.kabir63@gmail.com>
2024-01-31 21:38:29 +0600

    first commit

---
+++ custom.js
@@ -0,0 +1,24 @@
+// condition
+let idGuy = 5;
+if (idGuy > 5) {
+    console.log("I cant");
+}
+else 
+    if (idGuy <= 5) {
+    console.log("I am up");
+}
+else {
+    console.log("bye");
+}
+// **********************
+// Array
+let price = [20, 12, 13, 14];
+console.log(price.length);
+// first the var then . then length or indexof
+console.log (price.indexOf(13));
+// **********************
+// loop while and for
+let nmbr = 0;
+while (nmbr < 8) {
+    console.log ("start doing");
+}
